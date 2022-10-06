8b937436f66031de093ff7c7f60ff0b0f16853ca
Sharon Rossely CR <schullunquia@unsa.edu.pe>
2022-10-06 16:32:35 -0500

    add results

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app.config["DEBUG"] = True
 def home():
   return render_template('index.html')
 
+@app.route("/search", methods=['POST'])
+def search():
+  word = request.form['word-input']
+  return results(word)
+
+@app.route("/results/<word>")
+def results(word):
+  list_results = [word, word, word]
+  return render_template('results.html', list_results = list_results)
+
 if __name__ == "__main__":
   app.run()
