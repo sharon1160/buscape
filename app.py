4f8baa759832543eb73595c7065d2a7b33411c8b
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-10 09:33:36 -0500

    add flask_paginate for pagination of results

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask_paginate import Pagination, get_page_args
 import app_init
 import asyncio
 import concurrent.futures
@@ -42,33 +43,72 @@ async def results_range(word, list_results, event_loop):
 def sort_by_pagerank(list_results):
     return sorted(list_results, key=lambda d: d['page_rank'], reverse=True)
 
+def get_results(list_results, offset=0, per_page=10):
+    return list_results[offset: offset + per_page]
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
 
-@app.route("/search", methods=['POST'])
+list_results = []
+word = ''
+
+@app.route("/search", methods=['POST', 'GET'])
 def search():
-    word = request.form['word-input']
+    global list_results
+    global word
+    if request.method == 'POST':
+        word = request.form['word-input']
 
-    list_results = []
+        if word != '':
 
-    ###
-    t0 = time.time()
-    event_loop = asyncio.new_event_loop()
-    try:
-        event_loop.run_until_complete(
-            results_range(word, list_results, event_loop))
-    finally:
-        event_loop.close()
-        print('Time elapsed:', (time.time() - t0), 'seconds')
-    ###
+            list_results = []
 
-    list_results = sort_by_pagerank(list_results)
+            ###
+            t0 = time.time()
+            event_loop = asyncio.new_event_loop()
+            try:
+                event_loop.run_until_complete(
+                    results_range(word, list_results, event_loop))
+            finally:
+                event_loop.close()
+                print('Time elapsed:', (time.time() - t0), 'seconds')
+            ###
 
-    return render_template('results.html', results_list=list_results, n_results=len(list_results), word=word)
+            list_results = sort_by_pagerank(list_results)
 
+            page, per_page, offset = get_page_args(page_parameter="page",per_page_parameter="per_page")
+
+            total = len(list_results)
+
+            pagination_results = get_results(list_results, offset = offset, per_page = per_page)
+
+            pagination = Pagination(page = page, per_page = per_page, total = total, css_framework = 'bootstrap5')
+
+            return render_template('results.html',
+                                    n_results = total,
+                                    word = word,
+                                    pagination_results = pagination_results,
+                                    page = page,
+                                    per_page = per_page,
+                                    pagination = pagination)
+    else:
+        page, per_page, offset = get_page_args(page_parameter="page",per_page_parameter="per_page")
+
+        total = len(list_results)
+
+        pagination_results = get_results(list_results, offset = offset, per_page = per_page)
+
+        pagination = Pagination(page = page, per_page = per_page, total = total, css_framework = 'bootstrap5')
+
+        return render_template('results.html',
+                                n_results = total,
+                                word = word,
+                                pagination_results = pagination_results,
+                                page = page,
+                                per_page = per_page,
+                                pagination = pagination)
 
 if __name__ == "__main__":
     app.run()
