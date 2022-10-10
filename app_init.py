754e7131c97ba0e5a41a0707642683786c4fc67a
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-10 06:18:09 -0500

    add credentials

--- app_init.py
+++ app_init.py
@@ -1,7 +1,7 @@
 import firebase_admin
 
 cred_path = 'credentials.json'
-db_URL = 'https://wikisearcher-3d8ed-default-rtdb.firebaseio.com/'
+db_URL = 'https://crmb-333bb-default-rtdb.firebaseio.com/'
 
 credentials = firebase_admin.credentials.Certificate(cred_path)
 app = firebase_admin.initialize_app(credentials, {
